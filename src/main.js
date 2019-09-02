146310397c01a8224aec301b784dd2ca4149d0db
huojianhuanxiong <740216433@qq.com>
2019-09-02 20:05:43 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: []
+})
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
+
+export default router
